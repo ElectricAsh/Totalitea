0caa7aa7ab9e203144b03010f349de1c7b82271d
Inamullah969 <57355605+Inamullah969@users.noreply.github.com>
2019-11-07 11:16:51 +0000

    Add files via upload

--- TotaliteaV9_1.sql
+++ TotaliteaV9_1.sql
@@ -133,31 +133,31 @@ DECLARE
 	auser INT;
 	emcmsg VARCHAR2(120);
 BEGIN
-	insertProducts('Herbal', 'Green Tea', 'One of the healthiest beverages on the planet.', 'Reduces risk of cardiovascular diseases', 38, 'Drink', '', 'Clipper', 3.00, 160, 'No Nuts', 'No Dairy', 0.0, 0.0, aprod);
-	insertProducts('Caffeinated', 'Turkish Black Tea', 'Incredibly strong black tea', 'Reduces risk of diabetes', 0, 'Drink', '', 'Caykur', 6.10, 500, 'No Nuts', 'No Dairy', 0.0, 0.0, aprod);
-	insertProducts('Caffeinated', 'Americano', 'Hot water added to a shot of espresso', 'Holds essential nutrients and antioxidants', 83, 'Drink', '', 'Azera', 3.00, 100, 'No Nuts', 'No Dairy', 0.0, 0.0, aprod);
-	insertProducts('Caffeinated', 'Latte Macchiato', 'Espresso topped with foamed milk', 'Improves the mood of the consumer', 150, 'Drink', '', 'Tassimo', 3.98, 264, 'No Nuts', 'No Dairy', 7.0, 0.0, aprod);
-	insertProducts('Caffeinated', 'Mocha', 'Like a latte, but with added chocolate flavouring', 'None', 165, 'Drink', '', 'Nescafe', 2.65, 233, 'No Nuts', 'No Dairy', 25.0, 0.0, aprod);
-	insertProducts('Frosted cake', 'Red Velvet', 'Red velvet cake with a cream cheese frosting', 'None', 0, 'Cake', 'Latte', 'Sainsburys', 2.00, 350, 'No Nuts', 'Has Dairy', 38.5, 10.2, aprod);
+	insertProducts('Herbal', 'Green Tea', 'Tea - One of the healthiest beverages on the planet.', 'Reduces risk of cardiovascular diseases', 38, 'Drink', '', 'Clipper', 3.00, 160, 'No Nuts', 'No Dairy', 0.0, 0.0, aprod);
+	insertProducts('Caffeinated', 'Turkish Black Tea', 'Tea - Incredibly strong black tea', 'Reduces risk of diabetes', 0, 'Drink', '', 'Caykur', 6.10, 500, 'No Nuts', 'No Dairy', 0.0, 0.0, aprod);
+	insertProducts('Caffeinated', 'Americano', 'Coffee - Hot water added to a shot of espresso', 'Holds essential nutrients and antioxidants', 83, 'Drink', '', 'Azera', 3.00, 100, 'No Nuts', 'No Dairy', 0.0, 0.0, aprod);
+	insertProducts('Caffeinated', 'Latte Macchiato', 'Coffee - Espresso topped with foamed milk', 'Improves the mood of the consumer', 150, 'Drink', '', 'Tassimo', 3.98, 264, 'No Nuts', 'No Dairy', 7.0, 0.0, aprod);
+	insertProducts('Caffeinated', 'Mocha', 'Coffee - Like a latte, but with added chocolate flavouring', 'None', 165, 'Drink', '', 'Nescafe', 2.65, 233, 'No Nuts', 'No Dairy', 25.0, 0.0, aprod);
+	insertProducts('Frosted cake', 'Red Velvet', 'Cake - Red velvet cake with a cream cheese frosting', 'None', 0, 'Cake', 'Latte', 'Sainsburys', 2.00, 350, 'No Nuts', 'Has Dairy', 38.5, 10.2, aprod);
 	
 	insertUser('Stuart', '02-JUN-88', 'thestuart@meantinc.com', 'horse', '62 Boat Lane, Resolven, SA11 6NG', 1, to_Date('01/11/2019 09:30:00', 'DD/MM/YYYY HH24:MI:SS'), auser, emcmsg);
 	DBMS_OUTPUT.PUT_LINE(emcmsg);
-	insertProducts('Caffeinated', 'Espresso', 'Full-flavoured, concentrated form of coffee.', 'Improves concentration', 212, 'Drink', '', 'Azera', 4.20, 100, 'No Nuts', 'No Dairy', 0.0, 0.0, aprod); 
+	insertProducts('Caffeinated', 'Espresso', 'Coffee - Full-flavoured, concentrated form of coffee.', 'Improves concentration', 212, 'Drink', '', 'Azera', 4.20, 100, 'No Nuts', 'No Dairy', 0.0, 0.0, aprod); 
 	insertOrders(seq_orderid.nextval, aprod, auser, to_Date('04/11/2019 15:10:00', 'DD/MM/YYYY HH24:MI:SS'));
-	insertProducts('Herbal', 'Earl Grey', 'One of the most recognised flavoured teas', 'Improves digestion', 95, 'Drink', '', 'Twinings', 4.00, 250, 'No Nuts', 'No Dairy', 0.0, 0.0, aprod); 
+	insertProducts('Herbal', 'Earl Grey', 'Tea - One of the most recognised flavoured teas', 'Improves digestio', 95, 'Drink', '', 'Twinings', 4.00, 250, 'No Nuts', 'No Dairy', 0.0, 0.0, aprod); 
 	insertOrders(seq_orderid.currval, aprod, auser, to_Date('04/11/2019 15:10:00', 'DD/MM/YYYY HH24:MI:SS'));
-	insertProducts('Caffeinated', 'Cappuccino', 'A coffee drink typically composed of double espresso and hot milk', 'Source of useful antioxidants', 143, 'Drink', '', 'Azera', 14.00, 560, 'No Nuts', 'Has Dairy', 5.9, 1.4, aprod); 
+	insertProducts('Caffeinated', 'Cappuccino', 'Coffee - A drink typically composed of double espresso and hot milk.', 'Source of useful antioxidants', 143, 'Drink', '', 'Azera', 14.00, 560, 'No Nuts', 'Has Dairy', 5.9, 1.4, aprod); 
 	insertOrders(seq_orderid.currval, aprod, auser, to_Date('04/11/2019 15:10:00', 'DD/MM/YYYY HH24:MI:SS'));
-	insertProducts('Chocolate cake', 'Chocolate Celebration Cake', 'Moist, delicious and packed full of chocolate.', 'None', 0, 'Cake', 'Cappuccino', 'Thorntons', 2.20, 600, 'No Nuts', 'Has Dairy', 102.4, 47.4, aprod);
+	insertProducts('Chocolate cake', 'Chocolate Celebration Cake', 'Cake - Moist, delicious and packed full of chocolate.', 'None', 0, 'Cake', 'Cappuccino', 'Thorntons', 2.20, 600, 'No Nuts', 'Has Dairy', 102.4, 47.4, aprod);
 	insertOrders(seq_orderid.currval, aprod, auser, to_Date('04/11/2019 15:10:00', 'DD/MM/YYYY HH24:MI:SS'));
 	
 	insertUser('JamieG', '02-sep-88', 'jamiegustav@socialbounty.com', 'van', '96 Stroud Road, Old Craighill, EH22 3ZL', 0, to_Date('03/11/2019 10:30:00', 'DD/MM/YYYY HH24:MI:SS'), auser, emcmsg);
 	DBMS_OUTPUT.PUT_LINE(emcmsg);
-	insertProducts('Sponge cake', 'Victoria Sponge Cake', 'Two layers of sponge filled with cream and jam.', 'None', 0, 'Cake', 'Mocha', 'The Craft Company', 2.75, 345, 'No Nuts', 'Has Dairy', 81.0, 18.0, aprod); 
+	insertProducts('Sponge cake', 'Victoria Sponge Cake', 'Cake - Two layers of sponge filled with cream and jam.', 'None', 0, 'Cake', 'Mocha', 'The Craft Company', 2.75, 345, 'No Nuts', 'Has Dairy', 81.0, 18.0, aprod); 
 	insertOrders(seq_orderid.nextval, aprod, auser, to_Date('07/11/2019 17:10:00', 'DD/MM/YYYY HH24:MI:SS'));
-	insertProducts('Chocolate Cake', 'Belgian Chocolate Fudge Cake', 'Chocolate cake topped with belgian chocolate buttercream.', 'None', 0, 'Cake', 'Espresso', 'The Handmade Cake Company', 2.75, 395, 'No Nuts', 'Has Dairy', 115.2, 27.0, aprod);
+	insertProducts('Chocolate Cake', 'Belgian Chocolate Fudge Cake', 'Cake - Chocolate cake topped with belgian chocolate buttercream.', 'None', 0, 'Cake', 'Espresso', 'The Handmade Cake Company', 2.75, 395, 'No Nuts', 'Has Dairy', 115.2, 27.0, aprod);
 	insertOrders(seq_orderid.currval, aprod, auser, to_Date('07/11/2019 17:10:00', 'DD/MM/YYYY HH24:MI:SS'));
-	insertProducts('Caffeinated', 'Latte', 'Half coffee - Half milk', 'A latte a day keeps the doctor way.', 77, 'Drink', '', 'LavAzza', 13.93, 630, 'No Nuts', 'Has Dairy', 18.8, 14.4, aprod);   
+	insertProducts('Caffeinated', 'Latte', 'Coffee - Half coffee - Half milk', 'A latte a day keeps the doctor way.', 77, 'Drink', '', 'LavAzza', 13.93, 630, 'No Nuts', 'Has Dairy', 18.8, 14.4, aprod);   
 	insertOrders(seq_orderid.currval, aprod, auser, to_Date('07/11/2019 17:10:00', 'DD/MM/YYYY HH24:MI:SS'));
 	
 END;
